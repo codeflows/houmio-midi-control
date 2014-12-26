7ddba551ddc3946e755253d00b30adc8f3a5b6f2
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-26 10:11:27 +0200

    Start using WebSockets, break things

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var midi = require('midi')
-var request = require('request')
-var Bacon = require('baconjs')
+var midi = require("midi")
+var WebSocket = require("ws")
+var Bacon = require("baconjs")
 
 var siteKey = process.env.HORSELIGHTS_SITEKEY
 var lightId = process.argv[2]
@@ -14,7 +14,7 @@ console.log("Controlling lightId", lightId);
 function putLightState(state) {
   console.log("Putting state", state)
   var url = "https://houm.herokuapp.com/api/site/" + siteKey + "/light/state"
-  request({ method: 'PUT', url: url, body: state, json: true }, function (error, response, body) {
+  request({ method: "PUT", url: url, body: state, json: true }, function (error, response, body) {
     if(error) {
       console.log("Failed putting light state to", state)
     }
@@ -36,10 +36,27 @@ function toHoumioLightState(midiMessage) {
   }
 }
 
+// TODO error handling, reconnection etc
+function houmioConnection(messagesToHoumioStream) {
+  var socket = new WebSocket("wss://houm.herokuapp.com")
+  socket.on("open", function() {
+    console.log("Connected to Houm.io")
+    var subscribe = JSON.stringify({ command: "subscribe", data: { sitekey: siteKey } })
+    socket.send(subscribe)
+    Bacon.interval(3000).onValue(function() {
+      socket.ping(null, {}, false)
+    })
+  })
+  socket.on("close", function() { console.log("Websocket closed") })
+  socket.on("error", function() { console.log("Websocket error") })
+  socket.on("ping", socket.pong)
+  return Bacon.fromEventTarget(socket, "message", function(message) { return message.data  })
+}
+
 var input = new midi.input()
 
 var midiMessages =
-  Bacon.fromEventTarget(input, 'message', function(deltaTime, message) { return message })
+  Bacon.fromEventTarget(input, "message", function(deltaTime, message) { return message })
 
 midiMessages
   .filter(isControlMessage)
@@ -48,3 +65,6 @@ midiMessages
   .onValue(putLightState)
 
 input.openPort(0)
+
+houmioConnection(null)
+  .onValue(function(m) { console.log(m) })
